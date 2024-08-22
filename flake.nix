e7d35ca302b198c3b5cf4dd86e8f592c43970501
Hallam Smith <me@hcssmith.com>
2024-08-22 09:03:43 +0100

    Flow through nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,15 +2,24 @@
   description = "A very basic flake";
 
   inputs = {
-    flake-lib.url = "github:hcssmith/flake-lib";
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-lib = {
+      url = "github:hcssmith/flake-lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-lib }: flake-lib.lib.mkApp {
-  	inherit self;
-  	name = "e2fsprogs";
-  	drv = pkgs: pkgs.e2fsprogs.overrideAttrs(_: {
-  		doCheck = false;
-  	});
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-lib,
+  }:
+    flake-lib.lib.mkApp {
+      inherit self;
+      name = "e2fsprogs";
+      drv = pkgs:
+        pkgs.e2fsprogs.overrideAttrs (_: {
+          doCheck = false;
+        });
+    };
 }
